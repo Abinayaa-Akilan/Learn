c9de63c23ab1cbfa40d8f3c02f148e271f878a62
Abinayaa Akilan <akilanabinayaa@gmail.com>
2026-07-15 00:44:22 +0530

    Create Spring Boot Project with Spring Initializer
    Made an API localhost:8080, context path

---
+++ Myclass.java
@@ -0,0 +1,11 @@
+package com.example.FirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController public class Myclass {
+    @GetMapping("abc")
+    public String sayHello(){
+        return "Hello";
+    }
+}
